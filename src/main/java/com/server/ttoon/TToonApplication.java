1df56814ff9ab67ea581fc8138030cc11c7eb066
minjae <wjsalswp303@gmail.com>
2024-04-01 19:27:55 +0900

    [Setting] 프로젝트 생성

---
+++ src/main/java/com/server/ttoon/TToonApplication.java
@@ -0,0 +1,13 @@
+package com.server.ttoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TToonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TToonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/ttoon/TToonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.ttoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TToonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
