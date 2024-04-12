2f132a3bc259af6d1d2d2af629fc86dc0b6f8c6d
이찬희 <cksgml019@naver.com>
2024-04-12 22:49:01 +0900

    [FEATURE] SecurityUtil 구현 후 적용

--- src/main/java/com/server/ttoon/domain/member/controller/AppAuthController.java
+++ src/main/java/com/server/ttoon/domain/member/controller/AppAuthController.java
@@ -1,10 +1,14 @@
 package com.server.ttoon.domain.member.controller;
 
+import com.server.ttoon.common.exception.CustomRuntimeException;
 import com.server.ttoon.common.response.ApiResponse;
+import com.server.ttoon.common.response.status.ErrorStatus;
 import com.server.ttoon.domain.member.entity.Member;
+import com.server.ttoon.domain.member.repository.MemberRepository;
 import com.server.ttoon.security.auth.PrincipalDetails;
 import com.server.ttoon.security.jwt.dto.request.OAuth2LoginReqDto;
 import com.server.ttoon.domain.member.service.AppAuthService;
+import com.server.ttoon.security.util.SecurityUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,6 +17,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import static com.server.ttoon.common.response.status.ErrorStatus.MEMBER_NOT_FOUND_ERREOR;
+
 @Tag(name = "앱 로그인 API", description = "로그인 및 회원가입 기능")
 @RestController
 @RequestMapping("api")
@@ -20,6 +26,7 @@ import org.springframework.web.bind.annotation.*;
 public class AppAuthController {
 
     private final AppAuthService appAuthService;
+    private final MemberRepository memberRepository;
 
     @Operation(summary = "로그인", description = "RequestBody 로 provider, providerId 받아서 검증 후 로그인합니다.")
     @PostMapping("/auth/app/login")
@@ -30,9 +37,11 @@ public class AppAuthController {
 
     @Operation(summary = "회원가입", description = "이용 약관 동의 후 회원의 권한을 USER 로 변경합니다.")
     @PostMapping("/join")
-    public ResponseEntity<ApiResponse<?>> appJoin(@AuthenticationPrincipal PrincipalDetails principalDetails){
+    public ResponseEntity<ApiResponse<?>> appJoin(){
 
-        Member member = principalDetails.getMember();
+        Long currentMemberId = SecurityUtil.getCurrentMemberId();
+        Member member = memberRepository.findById(currentMemberId)
+                .orElseThrow(() -> new CustomRuntimeException(MEMBER_NOT_FOUND_ERREOR));
         return appAuthService.join(member);
     }
 }
---
+++ src/main/java/com/server/ttoon/security/util/SecurityUtil.java
@@ -0,0 +1,26 @@
+package com.server.ttoon.security.util;
+
+
+import com.server.ttoon.common.exception.CustomRuntimeException;
+import com.server.ttoon.common.response.status.ErrorStatus;
+import com.server.ttoon.security.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.security.spec.EdDSAParameterSpec;
+
+public class SecurityUtil {
+    private SecurityUtil() { }
+
+    // SecurityContext 에 유저 정보가 저장되는 시점
+    // Request 가 들어올 때 JwtFilter 의 doFilter 에서 저장
+    public static Long getCurrentMemberId() {
+
+        final PrincipalDetails principalDetails = (PrincipalDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principalDetails.getMember() == null || principalDetails.getUsername() == null)
+            throw new CustomRuntimeException(ErrorStatus.MEMBER_NOT_FOUND_ERREOR);
+
+        return Long.parseLong(principalDetails.getUsername());
+    }
+}
