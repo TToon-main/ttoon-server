c196d770bb905210296a02e17d1a54755a468b1f
minjae <wjsalswp303@gmail.com>
2024-04-12 18:33:47 +0900

    [FEATURE] Swagger 의존성 추가 및 설정

--- src/main/java/com/server/ttoon/domain/member/controller/AppAuthController.java
+++ src/main/java/com/server/ttoon/domain/member/controller/AppAuthController.java
@@ -5,11 +5,15 @@ import com.server.ttoon.domain.member.entity.Member;
 import com.server.ttoon.security.auth.PrincipalDetails;
 import com.server.ttoon.security.jwt.dto.request.OAuth2LoginReqDto;
 import com.server.ttoon.domain.member.service.AppAuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "앱 로그인 API", description = "로그인 및 회원가입 기능")
 @RestController
 @RequestMapping("api")
 @RequiredArgsConstructor
@@ -17,12 +21,14 @@ public class AppAuthController {
 
     private final AppAuthService appAuthService;
 
+    @Operation(summary = "로그인", description = "RequestBody 로 provider, providerId 받아서 검증 후 로그인합니다.")
     @PostMapping("/auth/app/login")
     public ResponseEntity<ApiResponse<?>> appLogin(@RequestBody OAuth2LoginReqDto oAuth2LoginReqDto){
 
         return appAuthService.login(oAuth2LoginReqDto);
     }
 
+    @Operation(summary = "회원가입", description = "이용 약관 동의 후 회원의 권한을 USER 로 변경합니다.")
     @PostMapping("/join")
     public ResponseEntity<ApiResponse<?>> appJoin(@AuthenticationPrincipal PrincipalDetails principalDetails){
 
