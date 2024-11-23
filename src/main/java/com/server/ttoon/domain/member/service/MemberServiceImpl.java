9857bbcce550a807834bdf2a6a75d8c65b6dbfb8
이찬희 <cksgml019@naver.com>
2024-11-23 16:26:43 +0900

    [REFACTOR] 애플설정 정보 변경

--- src/main/java/com/server/ttoon/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/server/ttoon/domain/member/service/MemberServiceImpl.java
@@ -410,7 +410,7 @@ public class MemberServiceImpl implements MemberService{
             return response.getBody();
         } catch (HttpClientErrorException e) {
             String errorMessage = e.getResponseBodyAsString(); // Apple 서버에서 반환한 에러 메시지
-            log.error("Apple Auth Token Error: {}", errorMessage); // 로그 기록
+            log.error("Apple Auth Token Error:{}", errorMessage); // 로그 기록
             throw new IllegalArgumentException("Apple Auth Token Error");
         }
     }
